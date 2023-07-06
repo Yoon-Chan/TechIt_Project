fc7dd5ac99f6b6dd18ab60fdfab108e5203dd703
Yoon Chan <vsvx13@naver.com>
2023-07-06 10:01:26 +0900

    뷰바인딩 설정, 메인 액티비티 툴바, 리사이클러뷰 추가, 메인 메뉴 옵션 추가, 액티비티 툴바 설정 및 메뉴 기능 구현

--- MemoApp/app/src/main/java/com/example/memoapp/MainActivity.kt
+++ MemoApp/app/src/main/java/com/example/memoapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.memoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.memoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.add -> {
+                Toast.makeText(this, "add menu 클릭", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
