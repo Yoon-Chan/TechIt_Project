6c913a0b0ffaa61df72ab88fd9e50e4f3cbca2bd
Yoon Chan <vsvx13@naver.com>
2023-07-07 14:16:22 +0900

    메모 작성 기능 구현(제목 editText 포커스 주기, 빈 값 있을 시 토스트로 빈값 채우기 알리기, 작성 완료(툴바 저장 클릭 시) 인텐트로 데이터 메인 액티비티에 전달하기, 메인 엑티비티는 전달받은 데이터 확인

--- MemoApp/app/src/main/java/com/example/memoapp/MainActivity.kt
+++ MemoApp/app/src/main/java/com/example/memoapp/MainActivity.kt
@@ -3,14 +3,28 @@ package com.example.memoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.memoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    private val requestLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if(it.resultCode == RESULT_OK){
+            val title = it.data?.getStringExtra("title")
+            val content = it.data?.getStringExtra("content")
+            val date = it.data?.getStringExtra("date")
+
+            Log.d("result", "title : $title\ncontent : $content\ndate : $date")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             R.id.add -> {
                 Toast.makeText(this, "add menu 클릭", Toast.LENGTH_LONG).show()
                 val intent = Intent(this, memoActivity::class.java)
-                startActivity(intent)
+                requestLauncher.launch(intent)
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- MemoApp/app/src/main/java/com/example/memoapp/memoActivity.kt
+++ MemoApp/app/src/main/java/com/example/memoapp/memoActivity.kt
@@ -2,13 +2,21 @@ package com.example.memoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.memoapp.databinding.ActivityMemoBinding
+import java.time.LocalDate
+import kotlin.concurrent.thread
 
 class memoActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMemoBinding
-
+    private lateinit var imm : InputMethodManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMemoBinding.inflate(layoutInflater)
@@ -16,6 +24,28 @@ class memoActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.memoToolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+        //titleEditText 포커스 요청 및 패드 올리기
+        requestFocus(binding.titleEditText)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.memo_create, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun requestFocus(view : View){
+        view.requestFocus()
+        thread {
+            SystemClock.sleep(500)
+            imm.showSoftInput(view, 0)
+        }
+    }
+
+    private fun hideFocus(){
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        currentFocus?.clearFocus()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -24,8 +54,37 @@ class memoActivity : AppCompatActivity() {
                 finish()
                 true
             }
-            else -> super.onOptionsItemSelected(item)
+            //세이브 버튼을 눌렀을 때
+            R.id.save -> {
+                //포커스 지우기
+                hideFocus()
 
+
+
+                val title = binding.titleEditText.text.toString()
+                val content = binding.contentEditTxt.text.toString()
+
+                //Log.e("content", "${content.isBlank()}")
+
+                //둘 중 하나 비어있는 경우 다시 입력 받도록 한다.
+                if(title.isEmpty() || content.isEmpty() || content.isBlank()){
+                    Toast.makeText(this, "빈 값을 있으면 안됩니다.", Toast.LENGTH_LONG).show()
+                    requestFocus(binding.titleEditText)
+
+                }else{
+                    val local = LocalDate.now().toString()
+
+                    //데이터 전달하기
+                    intent.putExtra("title", title)
+                    intent.putExtra("content", content)
+                    intent.putExtra("date", local)
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
 
         }
     }
